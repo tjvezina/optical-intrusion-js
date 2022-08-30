26256f61c0cbb7c12740585a4be19270fc26f18d
Tyler Vezina <tylerjvezina@gmail.com>
2022-08-30 00:27:42 -0400

    Imported p5.collide2D, added type definitions as needed

--- global.d.ts
+++ global.d.ts
@@ -4,3 +4,10 @@ import * as p5Global from 'p5/global';
 import module = require('p5');
 export = module;
 export as namespace p5;
+
+declare global {
+  // p5.collide2D
+  function collideCircleCircle(x1: number, y1: number, diam1: number, x2: number, y2: number, diam2: number): boolean;
+  function collideRectRect(x1: number, y1: number, w1: number, h1: number, x2: number, y2: number, w2: number, h2: number): boolean;
+  function collideRectCircle(rx: number, ry: number, rw: number, rh: number, cx: number, cy: number, diam: number): boolean;
+}
